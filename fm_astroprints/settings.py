20c2f0296cd2446ca6440f69cbed89116176e153
seanjcarley <seanjcarley@outlook.com>
2020-07-22 20:14:12 +0000

    added banner and search bar to base.html and updated base.css

--- fm_astroprints/settings.py
+++ fm_astroprints/settings.py
@@ -164,8 +164,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-print(STATICFILES_DIRS)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-print(MEDIA_ROOT)
