37f3524dee9e02fc5fdb4c871fd70b66673ec571
seanjcarley <seanjcarley@outlook.com>
2020-07-23 17:44:12 +0000

    added images models,fixtures,  and images to media folder

---
+++ images/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import Image, Image_Data
+
+# Register your models here.
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'img_title',
+        'img_taken',
+        'base_price',
+        'user_id',
+        'img_status',
+        'vol_sold',
+        'img_rating',
+        'img_data_id',
+    )
+
+    ordering = ('img_title',)
+
+
+class Images_DataAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'make',
+        'model',
+        'focal_length',
+        'aperture',
+        'shutter_speed_sec',
+        'iso',
+        'country',
+        'city',
+    )
+
+    ordering = ('make',)
+
+
+admin.site.register(Image, ImagesAdmin)
+admin.site.register(Image_Data, Images_DataAdmin)
---
+++ images/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Image(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    img_title = models.CharField(max_length=75, null=False, blank=False)
+    img_taken = models.DateField()
+    base_price = models.DecimalField(max_digits=6, decimal_places=2)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    img_status = models.IntegerField()
+    vol_sold = models.IntegerField()
+    img_rating = models.IntegerField(null=False, blank=False)
+    img_data_id = models.ForeignKey(
+        'Image_Data', to_field='id', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.id
+
+
+class Image_Data(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Image_Data'
+
+    make = models.CharField(max_length=50, null=True, blank=True)
+    model = models.CharField(max_length=50, null=True, blank=True)
+    focal_length = models.IntegerField()
+    aperture = models.CharField(max_length=10, null=True, blank=True)
+    shutter_speed_sec = models.IntegerField()
+    iso = models.IntegerField()
+    country = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+
+    # def __str__(self):
+    #     # print(self.img_title)
+    #     return self.id
