5254d915d5338eb12dfe6ddc33465dc9349ba5f8
seanjcarley <seanjcarley@outlook.com>
2020-08-09 13:25:20 +0000

    added update and remove functionality to shopping bag. also added increment and decrement

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.show_bag, name='show_bag'),
     path('add/<image_id>/', views.add_to_bag, name='add_to_bag'),
+    path(
+        'adjust/<image_id>/',
+        views.update_bag_quantities,
+        name='update_bag_quantities'),
     path('remove/<image_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
