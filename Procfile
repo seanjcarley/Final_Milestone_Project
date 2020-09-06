9a9655b3b8da7941f22c93712d249ef96c13197b
seanjcarley <seanjcarley@outlook.com>
2020-09-06 13:36:18 +0000

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fm_astroprints.wsgi:application
--- fm_astroprints/settings.py
+++ fm_astroprints/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'tiby701@5o@#p4#czs*l=o!kcv!z^wn&@zfrna%^91!l8j@suh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sc-finalmilestone-astroprint.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -134,12 +134,17 @@ WSGI_APPLICATION = 'fm_astroprints.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
