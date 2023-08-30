0d6ac7d7ad501dd318f1842a10dc47c8c6d04c9c
Sean <seanjcarley@gmail.com>
2023-08-30 15:22:30 +0100

    added psycopg2 to requirements.txt

--- fm_astroprints/settings.py
+++ fm_astroprints/settings.py
@@ -119,7 +119,7 @@ else:
     EMAIL_HOST = os.environ.get('EMAIL_HOST')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 #
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
